427589007bd7bb0e88c0549679786d04cef57e94
Teun Beijers <teunbeijers@acknowledge.nl>
2020-06-29 11:24:05 +0200

    extra build script updates and actually make some rules to see if it works

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 def main(args):
     with open(args.var_file, 'r') as fp:
@@ -13,12 +14,19 @@ def main(args):
     with open(args.temp_file, 'w') as fp:
         fp.writelines(combined_lines)
 
+    # @TODO: don't use os.system, it's lame and potentially unsafe
+    os.system('{0} -g --input-path {1} --output-path={2} -e'.format(
+        args.spirit, args.temp_file, args.output,
+    ))
+    print('Filter now available at: {0}'.format(args.output))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Filter Spirit variable helper')
     parser.add_argument('--var-file', '-v', required=True, help='The variable file you want to use')
     parser.add_argument('--filter', '-f', required=True, help='The filter file you want to use')
     parser.add_argument('--spirit', '-s', required=True, help='The path to the filter spirit executable')
+    parser.add_argument('--output', '-o', required=True, help='The path you want to output the final filter to')
     parser.add_argument('--temp-file', '-t', required=False, default='_filter.tmp', help='Path to temporary file to store combined filter in')
 
     main(parser.parse_args())
