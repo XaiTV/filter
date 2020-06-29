fb642c44b8a1619dda8d9cf924b9b3574ea0d4e4
Teun Beijers <teunbeijers@acknowledge.nl>
2020-06-29 12:41:04 +0200

    best I can make it for now

--- build.py
+++ build.py
@@ -1,13 +1,27 @@
 import argparse
 import os
 
+def get_var_from_line(line):
+    split_line = line.strip().split(' ')
+    if split_line[0].startswith('$'):
+        return split_line[0]
+    else:
+        return None
+
 def main(args):
     with open(args.var_base_file, 'r') as fp:
         var_lines = fp.readlines()
     
     if args.var_file:
+        new_lines = []
         with open(args.var_file, 'r') as fp:
-            var_lines += [''] + fp.readlines()
+            for line in fp.readlines() + var_lines:
+                var = get_var_from_line(line)
+                # if the var isn't used before, the line can be added
+                if not any(get_var_from_line(line2) == var for line2 in new_lines):
+                    new_lines.append(line)
+
+        var_lines = new_lines
 
     with open(args.filter, 'r') as fp:
         filter_lines = fp.readlines()
@@ -28,7 +42,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Filter Spirit variable helper')
     parser.add_argument('--var-base-file', '-b', default='base.vars', help='The variable file you want to use as a base')
-    parser.add_argument('--var-file', '-v', default='default.vars', help='The variable file you want to use for extra vars')
+    parser.add_argument('--var-file', '-v', required=False, help='The variable file you want to use to override base vars')
     parser.add_argument('--filter', '-f', default='xai.filter_spirit', help='The filter file you want to use')
     parser.add_argument('--spirit', '-s', required=True, help='The path to the filter spirit executable')
     parser.add_argument('--output', '-o', default='xai.filter', help='The path you want to output the final filter to')
