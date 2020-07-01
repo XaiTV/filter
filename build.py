22d9a6fff7c6263f893181739dd6650f61f0e214
teuneboon <teuneboon@gmail.com>
2020-07-01 18:51:32 +0200

    Allow having toggles in vars

--- build.py
+++ build.py
@@ -11,9 +11,11 @@ def get_var_from_line(line):
         return None
 
 
-def generate_filter_version(spirit, var_base_file, filter_file, output_path, var_file=None, toggle_file=None, temp_file='_filter.tmp'):
+def generate_filter_version(spirit, var_base_file, filter_file, output_path, var_file=None, temp_file='_filter.tmp'):
     with open(var_base_file, 'r') as fp:
         var_lines = fp.readlines()
+    
+    toggles = {}
     
     if var_file:
         new_lines = []
@@ -23,19 +25,15 @@ def generate_filter_version(spirit, var_base_file, filter_file, output_path, var
                 # if the var isn't used before, the line can be added
                 if var is None or not any(get_var_from_line(line2) == var for line2 in new_lines):
                     new_lines.append(line)
-
-        var_lines = new_lines
-
-    toggles = {}
-    if toggle_file:
-        with open(toggle_file, 'r') as fp:
-            for line in fp.readlines():
+                
                 line = line.strip()
-                if line.startswith('$'):
+                if line.startswith('#:'):
                     toggle_name, toggle_value = line.split('=')
-                    toggle_name = toggle_name.strip()[1:]
+                    toggle_name = toggle_name.strip()[2:]
                     toggle_value = toggle_value.strip()
                     toggles[toggle_name] = toggle_value
+
+        var_lines = new_lines
 
     with open(filter_file, 'r') as fp:
         filter_lines = []
@@ -76,36 +74,28 @@ def main(args):
     if args.var_directory:
         var_files += glob.glob(os.path.join(args.var_directory, '*.vars'))
 
-    toggle_files = [None]
-    if args.toggle_directory:
-        toggle_files += glob.glob(os.path.join(args.toggle_directory, '*.toggle'))
-
     output_path = os.path.split(args.output)[0]
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     for var_file in var_files:
-        for toggle_file in toggle_files:
-            output_name = '{0}'.format(args.output)
-            if toggle_file:
-                output_name += '.{0}'.format(os.path.split(toggle_file)[-1][:-7])
-            else:
-                output_name += '.generic'
+        output_name = '{0}'.format(args.output)
 
-            if var_file:
-                output_name += '.{0}'.format(os.path.split(var_file)[-1][:-5])
+        if var_file:
+            output_name += '.{0}'.format(os.path.split(var_file)[-1][:-5])
+        else:
+            output_name += '.generic'
 
-            output_name += '.filter'
+        output_name += '.filter'
 
-            generate_filter_version(
-                args.spirit,
-                args.var_base_file,
-                args.filter,
-                output_name,
-                var_file,
-                toggle_file,
-                args.temp_file,
-            )
+        generate_filter_version(
+            args.spirit,
+            args.var_base_file,
+            args.filter,
+            output_name,
+            var_file,
+            args.temp_file,
+        )
 
     print('All filters generated')
 
@@ -115,7 +105,6 @@ if __name__ == '__main__':
     parser.add_argument('--var-base-file', '-b', default='base.vars', help='The variable file you want to use as a base')
     parser.add_argument('--var-directory', '-v', required=False, help='The directory with .vars files in it to base filter variations on')
     parser.add_argument('--filter', '-f', default='xai.filter_spirit', help='The filter file you want to use')
-    parser.add_argument('--toggle-directory', '-t', required=False, help='The directory with toggle files in it')
     parser.add_argument('--spirit', '-s', required=True, help='The path to the filter spirit executable')
     parser.add_argument('--output', '-o', default='output/xai', help='The filter output path BEFORE the .filter. So for example: output/xai will become output/xai.generic.filter and output/xai.strict.filter.')
     parser.add_argument('--temp-file', default='_filter.tmp', help='Path to temporary file to store combined filter in')
